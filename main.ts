4a33448a57e40b014b52786f72d92d35d570244e
Eric Ong <ako.si.eric@gmail.com>
2021-04-06 08:44:54 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+radio.setGroup(1)
+basic.forever(function () {
+    radio.sendValue("string", 1)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.clearScreen()
+    basic.pause(1000)
+})
